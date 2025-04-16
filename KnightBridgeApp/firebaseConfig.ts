710c5900ec2a64d31a368354085622bf941c56c9
GravitB <86177559+GravitB@users.noreply.github.com>
2025-04-16 11:51:55 -0400

    Update firebaseConfig.ts

--- KnightBridgeApp/firebaseConfig.ts
+++ KnightBridgeApp/firebaseConfig.ts
@@ -1,16 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database"; // 👈 import this
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD-OI8YYYcvH5QmOK7dr94oa-KytJlfByU",
-    authDomain: "knightbridge-e6791.firebaseapp.com",
-    projectId: "knightbridge-e6791",
-    storageBucket: "knightbridge-e6791.firebasestorage.app",
-    messagingSenderId: "731608857119",
-    appId: "1:731608857119:web:d5f24e7b93a09f186ab32a",
-    measurementId: "G-G0B0C383DQ"
-  };
-  
+  apiKey: "AIzaSyD-OI8YYYcvH5QmOK7dr94oa-KytJlfByU",
+  authDomain: "knightbridge-e6791.firebaseapp.com",
+  databaseURL: "https://knightbridge-e6791-default-rtdb.firebaseio.com", // 👈 make sure this line is here
+  projectId: "knightbridge-e6791",
+  storageBucket: "knightbridge-e6791.appspot.com", // fixed `.app` typo
+  messagingSenderId: "731608857119",
+  appId: "1:731608857119:web:d5f24e7b93a09f186ab32a",
+  measurementId: "G-G0B0C383DQ"
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const db = getDatabase(app); // 👈 this is your realtime database
